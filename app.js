c656f82e778333487211c405a78792b2cf56fa07
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-26 23:52:46 +0000

    Added nodeJS package chalk and played a bit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const validator = require('validator');
-const add = require('./utils');
+const chalk = require('chalk');
 
-
-const sum = add (4, -2);
-
-console.log(sum);
 
 const getNotes = require('./notes');
 
 getNotes();
+
+console.log(chalk.blue('Here I am in Blue!'));
+
+const greenMsg = chalk.green('Success');
+const boldGreenMsg = chalk.green.bgRed.bold('Success');
+console.log(greenMsg);
+console.log(boldGreenMsg);
